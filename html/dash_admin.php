9304a5a79252843bd910d86822f33de97ebf570e
Nelga <72596243+Nelga-Repo@users.noreply.github.com>
2021-02-15 12:50:32 +0100

    v0.0.2
    Base maquette

---
+++ html/dash_admin.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Page type</title>
+</head>
+<body>
+
+    <header>
+        <!--Barre navigation quand connecté-->
+        <nav>
+            <img src="imgs/koala-1400413-1185985.png" alt="">
+            <a href="#" src="" alt="pic_koala"></a>
+            <p>Questions fréquente</p>
+            <P>Nom utilisateur</P>
+            <a href="#">Compte Client</a>
+            <a href="#" src="" alt="pic_profil"></a>
+        </nav>
+
+        <!--Partie bloc info-->
+        <div class="blocheader">
+            <div class="allblock">
+                <h3>Title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolores laudantium natus excepturi placeat sed dolorem vel cumque facilis. Illum!</p>
+                <div class="dateandarticle">
+                    <p>La date</p>
+                    <a href="#">Voir l'article</a>
+                </div>
+
+                <h3>Title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolores laudantium natus excepturi placeat sed dolorem vel cumque facilis. Illum!</p>
+                <div class="dateandarticle">
+                    <p>La date</p>
+                    <a href="#">Voir l'article</a>
+                </div>
+
+                <h3>Title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolores laudantium natus excepturi placeat sed dolorem vel cumque facilis. Illum!</p>
+                <div class="dateandarticle">
+                    <p>La date</p>
+                    <a href="#">Voir l'article</a>
+                </div>
+
+                <h3>Title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolores laudantium natus excepturi placeat sed dolorem vel cumque facilis. Illum!</p>
+                <div class="dateandarticle">
+                    <p>La date</p>
+                    <a href="#">Voir l'article</a>
+                </div>
+            </div>
+        </div>
+
+
+        
+    </header>
+    
+    <section>
+
+        <div class="currently">
+            <h2>Actuellement</h2>
+            <p>X tickets ouverts</p>
+            <p>Ouvrir une demande</p>
+        </div>
+
+        <div class="openticket">
+            <h2>Tickets ouverts</h2>
+            <div class="ticketinfo">
+                <ul>
+                    <li>Dernière réponse</li>
+                    <li>Sujet</li>
+                    <li>Création</li>
+                    <li>Numéro</li>
+                    <li>Etat du ticket</li>
+                </ul>
+            </div>
+            <div class="ticketgrey">
+                <div class="answeranddate" >
+                    <p>Réponse de XXXX</p>
+                    <p>Le telle date</p>
+                </div>
+                <p>impossible d'accéder a mon compte client</p>
+                <p>Telle date</p>
+                <p>#212251</p>
+                <div class="answered">
+                    <p>Répondu</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="closeticket">
+            <h2>Tickets ouverts</h2>
+            <div class="ticketinfo">
+                <ul>
+                    <li>Dernière réponse</li>
+                    <li>Sujet</li>
+                    <li>Création</li>
+                    <li>Numéro</li>
+                    <li>Etat du ticket</li>
+                </ul>
+            </div>
+            <div class="ticketgrey">
+                <div class="answeranddate" >
+                    <p>Réponse de XXXX</p>
+                    <p>Le telle date</p>
+                </div>
+                <p>impossible d'accéder a mon compte client</p>
+                <p>Telle date</p>
+                <p>#212251</p>
+                <div class="answered">
+                    <p>Répondu</p>
+                </div>
+            </div>
+        </div>
+
+    </section>
+</body>
+</html>
---
+++ html/dash_client.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/master.css">
+    <title>Koala Ticket</title>
+</head>
+<body>
+    <header class="header">
+        <div class="menu">
+            <div class="logo">
+                <img src="images/koala.png" alt="Logo du site">
+                <div class="line"></div>
+                <a href="">Questions fréquentes</a>
+            </div>
+            <div class="user">
+                <div class="user_info">
+                    <p class="user_name">Sébastien</p>
+                    <p class="user_account">Compte client</p>
+                </div>
+            </div>
+        </div>
+        <div class="articles">
+            <div class="article_box">
+                <p class="article_title">Lorem Ipsum</p>
+                <p class="article_desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore consequuntur labore recusandae dignissimos saepe at nobis! Minus, repellendus.....</p>
+            </div>
+            <div class="article_link">
+                <p class="article_date">Le 16/11/2020</p>
+                <a href="">Voir l'article -></a>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
