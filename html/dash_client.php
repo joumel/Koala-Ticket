52be50cdca55c5ac4ba5db413bdab400e4e966d5
Nelga-repo <contact@joumel.fr>
2021-02-15 15:11:36 +0100

    v0.0.3
    Header

--- html/dash_client.php
+++ html/dash_client.php
@@ -5,31 +5,63 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/master.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet"> 
     <title>Koala Ticket</title>
 </head>
 <body>
+    <!-- Affichage  de la navigation est des articles -->
     <header class="header">
         <div class="menu">
             <div class="logo">
                 <img src="images/koala.png" alt="Logo du site">
-                <div class="line"></div>
+                <div class="line_vertical"></div>
                 <a href="">Questions fréquentes</a>
             </div>
             <div class="user">
-                <div class="user_info">
-                    <p class="user_name">Sébastien</p>
-                    <p class="user_account">Compte client</p>
+                <div class="user_box">
+                    <div class="user_info">
+                        <p class="user_name">Sébastien</p>
+                        <p class="user_account">Compte client</p>
+                    </div>
+                    <div class="user_picture">
+                        <img src="images/user.jpg" alt="">
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="articles">
-            <div class="article_box">
-                <p class="article_title">Lorem Ipsum</p>
-                <p class="article_desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore consequuntur labore recusandae dignissimos saepe at nobis! Minus, repellendus.....</p>
+        <!-- Affichage des articles -->
+        <div class="line_horizontal"></div>
+        <div class="all_articles">
+            <div class="article">
+                <div class="article_box">
+                    <p class="article_title">Lorem Ipsum</p>
+                    <p class="article_desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore consequuntur labore recusandae dignissimos saepe at nobis! Minus, repellendus.....</p>
+                </div>
+                <div class="article_link">
+                    <p class="article_date">Le 16/11/2020</p>
+                    <a href="">Voir l'article -></a>
+                </div>
             </div>
-            <div class="article_link">
-                <p class="article_date">Le 16/11/2020</p>
-                <a href="">Voir l'article -></a>
+            <div class="article">
+                <div class="article_box">
+                    <p class="article_title">Lorem Ipsum</p>
+                    <p class="article_desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore consequuntur labore recusandae dignissimos saepe at nobis! Minus, repellendus.....</p>
+                </div>
+                <div class="article_link">
+                    <p class="article_date">Le 16/11/2020</p>
+                    <a href="">Voir l'article -></a>
+                </div>
+            </div>
+            <div class="article">
+                <div class="article_box">
+                    <p class="article_title">Lorem Ipsum</p>
+                    <p class="article_desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore consequuntur labore recusandae dignissimos saepe at nobis! Minus, repellendus.....</p>
+                </div>
+                <div class="article_link">
+                    <p class="article_date">Le 16/11/2020</p>
+                    <a href="">Voir l'article -></a>
+                </div>
             </div>
         </div>
     </header>
