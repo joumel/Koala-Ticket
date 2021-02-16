ce6059832fe44325acd87442a6f89794dee1bc3e
Pierre <grandmarg@hotmail.fr>
2021-02-16 15:56:04 +0100

    v1.0.2
    Main_controller & templates

---
+++ public/js/faq.js
@@ -0,0 +1,13 @@
+$ (function (){
+    $(".visible-pannel img").click(function() {
+
+        //Affichage du + ou -
+        if(this.src.includes('plus')){
+            this.src = "images/minus.png";
+            $(this).parent().next().slideDown("slow");
+        }else if (this.src.includes('minus')){
+            this.src = "images/plus.jpg";
+            $(this).parent().next().slideUp();
+        }
+    });
+});
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,12 +9,72 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/home", name="home")
      */
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'controller_home' => 'Page d\'accueil',
+        ]);
+    }
+
+    /**
+     * @Route("/article", name="article")
+     */
+    public function article(): Response
+    {
+        return $this->render('main/article.html.twig', [
+            'controller_article' => 'Page article',
+        ]);
+    }
+
+    /**
+     * @Route("/administrateur", name="dash_admin")
+     */
+    public function dash_admin(): Response
+    {
+        return $this->render('main/dash_admin.html.twig', [
+            'controller_dash_admin' => 'Page administrateur',
+        ]);
+    }
+
+    /**
+     * @Route("/client", name="dash_client")
+     */
+    public function dash_client(): Response
+    {
+        return $this->render('main/dash_client.html.twig', [
+            'controller_dash_client' => 'Page client',
+        ]);
+    }
+
+    /**
+     * @Route("/faq", name="faq")
+     */
+    public function faq(): Response
+    {
+        return $this->render('main/faq.html.twig', [
+            'controller_faq' => 'Page faq',
+        ]);
+    }
+
+    /**
+     * @Route("/connexion", name="login")
+     */
+    public function login(): Response
+    {
+        return $this->render('main/login.html.twig', [
+            'controller_login' => 'Page connexion',
+        ]);
+    }
+
+    /**
+     * @Route("/inscription", name="register")
+     */
+    public function register(): Response
+    {
+        return $this->render('main/register.html.twig', [
+            'controller_register' => 'Page inscription',
         ]);
     }
 }
