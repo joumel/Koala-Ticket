8a4c9ab577976e3edd2e8cf4adeb6b6287f5aa47
danielhrt <73801890+danielhrt@users.noreply.github.com>
2021-02-16 11:40:12 +0100

    page login register

---
+++ html/login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+    <title>Koala Ticket</title>
+</head>
+<body class="backgroundForm">
+<!--HEADER -->
+        <header class="header">
+                <div class="menu">
+                    <div class="logo">
+                        <img src="images/koala.png" alt="Logo du site">
+                        <div class="line_vertical"></div>
+                        <a href="">Questions fréquentes</a>
+                    </div>
+                    <div class="user">
+                        <div class="user_box">
+                            <div class="user_info">
+                                <p class="user_name">Sébastien</p>
+                                <p class="user_account">Compte client</p>
+                            </div>
+                            <div class="user_picture">
+                                <img src="images/user.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </header>
+        <!-- FORMS -->
+        <form action="" class="FormsLogReg">
+            <h1>Login</h1>
+            <label for="email">Email</label>
+            <input type="email">
+            <label for="password">Password</label>
+            <input type="password">
+            <input type="button" name="button" value="Login">
+        </form>
+</body>
+</html>
