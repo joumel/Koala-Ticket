c6d2a2837e53e5f1f714f74e217a30ec985120b8
Nelga-repo <contact@joumel.fr>
2021-02-16 15:44:01 +0100

    v1.0.1
    Add Form for register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use \DateTime;
 
 class RegistrationController extends AbstractController
 {
@@ -28,6 +29,16 @@ class RegistrationController extends AbstractController
                     $user,
                     $form->get('plainPassword')->getData()
                 )
+            );
+
+            // Set regitration datetime
+            $user->setlastConnexion(
+                new DateTime()
+            );
+
+            // Set initial suspended statut
+            $user->setsuspended(
+                0
             );
 
             $entityManager = $this->getDoctrine()->getManager();
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,21 +11,14 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -39,6 +32,46 @@ class RegistrationFormType extends AbstractType
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
+                    ]),
+                ],
+            ])
+
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci de renseigner votre prénom',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 50,
+                        'minMessage' => 'Votre prénom doit contenir {{ limit }} caractères minimum',
+                        'maxMessage' => 'Votre prénom ne peut contenir plus de {{ limit }} caractères',
+                    ]),
+                    ],
+            ])
+
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci de renseigner votre Nom',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 50,
+                        'minMessage' => 'Votre nom doit contenir {{ limit }} caractères minimum',
+                        'maxMessage' => 'Votre nom ne peut contenir plus de {{ limit }} caractères',
+                    ]),
+                ],
+            ])
+
+            ->add('email')
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez acceptez les conditions générales d\'utilisations',
                     ]),
                 ],
             ])
