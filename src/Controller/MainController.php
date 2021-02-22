c2b605596e2334e0f975965b9a06d739b572956a
Nivash <nivash_goodur@live.fr>
2021-02-22 15:20:53 +0100

    v1.3.2
    Petits correctifs W3C

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -105,6 +105,7 @@ class MainController extends AbstractController
         //Récupération des tickets fermés du client connecté
         $ticketsClosedClient = $ticketRepo->findby(['owner' => $userId , 'statement' => 'fermé'],['updateTime' => 'DESC']);
 
+        //Si l'utilisateur à le rôle admin, on le redirige vers le dashboard admin
         if ($userRoles[0] == 'ROLE_ADMIN') {
             return $this->redirectToRoute('dash_admin');
         };
@@ -203,6 +204,7 @@ class MainController extends AbstractController
 
         $userId = $this->getUser()->getId();
 
+        //Récupération des repository
         $messageRepo = $this->getDoctrine()->getRepository(Message::class);
         $ticketInfo = $this->getDoctrine()->getRepository(Ticket::class);
         $userInfo = $this->getDoctrine()->getRepository(User::class);
@@ -226,6 +228,7 @@ class MainController extends AbstractController
         // Envoi du formulaire
         $form->handleRequest($request);
 
+        //Si le formulaire est esnvoyé et valide on hydrate
         if($form->isSubmitted() && $form->isValid()){
 
             date_default_timezone_set('Europe/Paris');
